9e307244d9d9de29a5db0030767b5601e8fef45b
nabellaayug <nabellaayugiwanti@gmail.com>
2023-12-11 22:55:51 +0700

    add access control

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const app = express();
 app.use(express.json());
 
 app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "https://successful-clothes-ox.cyclic.app");
+  res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader("Access-Control-Allow-Methods", "POST", "GET", "PUT");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type");
   next();
