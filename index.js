ad3f39776d2c520112b5b1bf819bc9ebe8f0649a
nabellaayug <nabellaayugiwanti@gmail.com>
2023-12-12 05:59:48 +0700

    delete comment

--- index.js
+++ index.js
@@ -7,14 +7,6 @@ const app = express();
 
 app.use(express.json());
 app.use(cors());
-// app.use((req, res, next) => {
-//   res.setHeader('Access-Control-Allow-Origin', '*');
-//   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-//   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-//   res.setHeader('Access-Control-Allow-Credentials', 'true');
-//   next();
-// })
-
 app.use('/external-server', createProxyMiddleware({ target: 'https://successful-clothes-ox.cyclic.app', changeOrigin: true }));
 
 app.get("/test", (req, res) => {
