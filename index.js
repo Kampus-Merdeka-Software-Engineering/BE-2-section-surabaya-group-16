d4c504d52893e76322a7a40fe86d1947f319b73f
nabellaayug <nabellaayugiwanti@gmail.com>
2023-12-11 22:02:34 +0700

    add access control

--- index.js
+++ index.js
@@ -7,9 +7,12 @@ const app = express();
 app.use(express.json());
 
 app.use((req, res, next) => {
-  res.setHeader("Content-Type", "application/json");
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "POST", "GET", "PUT");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
   next();
-});
+})
+
 
 app.get("/test", (req, res) => {
   res.send("Test route");
